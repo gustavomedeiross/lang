5ffd3e3157be6ea766f93fa2b769690965863a88
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2023-08-09 20:16:59 -0300

    Add parser (lalrpop)

--- src/ast.rs
+++ src/ast.rs
@@ -4,7 +4,10 @@
 pub struct Id(String);
 
 #[derive(Debug, PartialEq, Clone)]
-pub struct Literal(String);
+pub enum Literal {
+    Int(i64),
+    Str(String)
+}
 
 // at some point we want to support patterns as arguments
 // (but not on lambdas probably)
@@ -16,14 +19,16 @@ pub enum ParsedExpr {
     Lit(Literal),
     // TODO: Vec<Box<T>> is redundant, so probably just use Vec<T>
     App(Box<ParsedExpr>, Vec<Box<ParsedExpr>>),
-    Let(Vec<Definition>, Box<ParsedExpr>),
+    // TODO: add support for multiple bindings in a single let
+    Let(Id, Box<ParsedExpr>, Box<ParsedExpr>),
+    // Let(Vec<Definition>, Box<ParsedExpr>),
     Lambda(Args, Box<ParsedExpr>),
 }
 
-#[derive(Debug, PartialEq, Clone)]
-pub enum Definition {
-    // TODO: maybe "Signature" would be a better name
-    TypeAnnotation(Id, Box<ParsedExpr>),
-    ValueDefinition(Id, Args, ParsedExpr),
-}
+// #[derive(Debug, PartialEq, Clone)]
+// pub enum Definition {
+//     // TODO: maybe "Signature" would be a better name
+//     TypeAnnotation(Id, Box<ParsedExpr>),
+//     ValueDefinition(Id, Args, Box<ParsedExpr>),
+// }
 
