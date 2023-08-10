8bbee7a75f139c4f07609e9167acbc23381bc970
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2023-08-10 20:12:41 -0300

    Add more parser tests

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
 
 #[cfg(test)]
 mod parser_tests {
-    use super::ast::{Literal, ParsedExpr};
+    use super::ast::{Id, Literal, ParsedExpr};
     use super::*;
 
     fn parse(input: &str) -> ParsedExpr {
@@ -22,8 +22,59 @@ mod parser_tests {
     }
 
     #[test]
-    fn test_1() {
-        let result = parse("1");
-        assert_eq!(result, ParsedExpr::Lit(Literal::Int(1)));
+    fn test_literals() {
+        assert_eq!(parse("1"), ParsedExpr::Lit(Literal::Int(1)));
+        assert_eq!(
+            parse(r#""hello""#),
+            ParsedExpr::Lit(Literal::Str(r#""hello""#.to_owned()))
+        );
+    }
+
+    #[test]
+    fn test_lambda() {
+        let result = parse("fun x -> x");
+        let expected = ParsedExpr::Lambda(
+            vec![Id("x".to_owned())],
+            Box::new(ParsedExpr::Var(Id("x".to_owned()))),
+        );
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_application_1() {
+        let result = parse("f x");
+        let expected = ParsedExpr::App(
+            Box::new(ParsedExpr::Var(Id("f".to_owned()))),
+            vec![Box::new(ParsedExpr::Var(Id("x".to_owned())))],
+        );
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_application_2() {
+        let result = parse("f x y");
+        let expected = ParsedExpr::App(
+            Box::new(ParsedExpr::Var(Id("f".to_owned()))),
+            vec![
+                Box::new(ParsedExpr::Var(Id("x".to_owned()))),
+                Box::new(ParsedExpr::Var(Id("y".to_owned()))),
+            ],
+        );
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_let() {
+        let result = parse("let x = 1 in x");
+        let expected = ParsedExpr::Let(
+            Id("x".to_owned()),
+            Box::new(ParsedExpr::Lit(Literal::Int(1))),
+            Box::new(ParsedExpr::Var(Id("x".to_owned()))),
+        );
+
+        assert_eq!(result, expected);
     }
 }
