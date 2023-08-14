0f358a9aca5623f35d022d1e64580ebb7a8c755a
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2023-08-13 21:48:12 -0300

    simplify function applications

--- src/simplifier.rs
+++ src/simplifier.rs
@@ -4,20 +4,20 @@ use crate::ast::{ParsedExpr, Expr};
 pub enum SimplifierError {}
 
 pub fn simplify(parsed_expr: ParsedExpr) -> Result<Expr<()>, SimplifierError> {
-// pub fn simplify(parsed_expr: ParsedExpr) -> Expr<()> {
     match parsed_expr {
         ParsedExpr::Var(id) =>  Ok(Expr::Var(id)),
         ParsedExpr::Lit(lit) => Ok(Expr::Lit(lit)),
         // TODO: do something
-        // ParsedExpr::App(e1, e2) => Ok(Expr::App(
-        //     // TODO: Box.map
-        //     Box::new(simplify(*e1)?),
-        //     Box::new(simplify(*e2)?),
-        // )),
-        ParsedExpr::App(_, _) => todo!(),
+        ParsedExpr::App(e, es) => {
+            let expr = simplify(*e)?;
+            es.into_iter().try_fold(expr, |acc, e| {
+                Ok(Expr::App(Box::new(acc), Box::new(simplify(*e)?)))
+            })
+        },
         ParsedExpr::Let(id, e1, e2) => Ok(Expr::Let(
             id,
             (),
+            // TODO: Box.map
             Box::new(simplify(*e1)?),
             Box::new(simplify(*e2)?),
         )),
@@ -35,7 +35,7 @@ mod tests {
     use crate::ast::{Expr, ParsedExpr, Id};
 
     #[test]
-    fn test_simplify_lambda() {
+    fn test_lambda() {
         // fun a b -> a
         let parsed_expr = ParsedExpr::Lambda(
             vec![Id("a".to_owned()), Id("b".to_owned())],
@@ -51,6 +51,33 @@ mod tests {
                 (),
                 Box::new(Expr::Var(Id("a".to_owned()))),
             )),
+        );
+
+        assert_eq!(simplify(parsed_expr), Ok(expected));
+    }
+
+    #[test]
+    fn test_app() {
+        // f a b c
+        let parsed_expr = ParsedExpr::App(
+            Box::new(ParsedExpr::Var(Id("f".to_owned()))),
+            vec![
+                Box::new(ParsedExpr::Var(Id("a".to_owned()))),
+                Box::new(ParsedExpr::Var(Id("b".to_owned()))),
+                Box::new(ParsedExpr::Var(Id("c".to_owned()))),
+            ]
+        );
+
+        // (((f a) b) c)
+        let expected = Expr::App::<()>(
+            Box::new(Expr::App::<()>(
+                Box::new(Expr::App::<()>(
+                    Box::new(Expr::Var(Id("f".to_owned()))),
+                    Box::new(Expr::Var(Id("a".to_owned()))),
+                )),
+                Box::new(Expr::Var(Id("b".to_owned()))),
+            )),
+            Box::new(Expr::Var(Id("c".to_owned()))),
         );
 
         assert_eq!(simplify(parsed_expr), Ok(expected));
